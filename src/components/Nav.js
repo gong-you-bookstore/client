e940f1b392a5de4c60c77b175525fbc5f3698930
jiheon788 <jiheon788@ajou.ac.kr>
2022-10-14 21:29:35 +0900

    style: Modify responsive Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,33 +1,62 @@
 import { useNavigate } from "react-router-dom"
+import $ from 'jquery'
 
 const Nav = ()=>{
   const navigate = useNavigate();
 
+  const onClickHamburger = () => {
+    if($('.hamburger').hasClass("active")){
+      $('.hamburger').removeClass( 'active' );
+      $('.navbar-menu').removeClass( 'active' );
+    } else {
+      $('.hamburger').addClass( 'active' );
+      $('.navbar-menu').addClass( 'active' );
+    }
+    
+  }
 
   return (
     <nav className="navbar">
-      <h1 className="nav-logo"
-        onClick={()=>{navigate("/")}}
-      >공 유 책 방</h1>
+      <div className="navbar-logo">
+        <h1
+          onClick={()=>{navigate("/")}}
+        >
+          공 유 책 방
+        </h1>
+      </div>
+      
+      <div class="hamburger" onClick={()=>onClickHamburger()}>
+        <span className="line"></span>
+        <span className="line"></span>
+        <span className="line"></span>
+      </div>
+      
 
       <ul className="navbar-menu">                     
-        <li className="nav-item">
+        <li>
           <a 
-            className="nav-link" 
             onClick={()=>{navigate("/bookstore")}}
-          >책방</a>
+          >
+            책방
+          </a>
         </li>
-
-        <li className="nav-item">
-          <a className="nav-link" onClick={()=>{navigate("/register")}}>등록</a>
+        <li>
+          <a 
+            onClick={()=>{navigate("/register")}}
+          >
+            등록
+          </a>
         </li>
-        
-        
-        <li className="nav-item">
-          <a className="nav-link"
-            onClick={()=>{navigate('/login')}}>login</a>
+        <li>
+          <a 
+            onClick={()=>{navigate('/login')}}
+          >
+            login
+          </a>
         </li>
       </ul>
+
+      
     </nav>
 
 
