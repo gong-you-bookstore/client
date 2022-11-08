bd0225e62574d3d9849d2f02ff68e33805ba05ef
jiheon788 <jiheon788@ajou.ac.kr>
2022-11-08 19:37:20 +0900

    chore: change env file

--- src/lib/services.js
+++ src/lib/services.js
@@ -1,20 +1,36 @@
 import axios from "axios";
 
-// 카카오 api: 검색어를 통하여 일치하는 책들의 리스트 얻음
+const KAKAO_HOST_URL = process.env.REACT_APP_KAKAO_HOST_URL
+const KAKAO_REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY
+const NLK_HOST_URL = process.env.REACT_APP_NLK_HOST_URL
+const NLK_API_KEY = process.env.REACT_APP_NLK_API_KEY
+
+// 카카오 API: 검색어를 통하여 일치하는 책들의 리스트 얻음
 export const getBooksByKakao = async (searchWord) => {
   return await axios({
     method: "get",
-    url: `https://dapi.kakao.com/v3/search/book?&size=20&query=${searchWord}`,
+    url: `${KAKAO_HOST_URL}/v3/search/book?&size=20&query=${searchWord}`,
     headers: {
-      Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_REST_API_KEY_JH}`
+      Authorization: `KakaoAK ${KAKAO_REST_API_KEY}`
     }
   });
 };
 
-// 국립중앙도서관 api: isbn값을 통하여 책의 정보 얻음
+// 국립중앙도서관 API: isbn값을 통하여 책의 정보 얻음
 export const getBookByNLK = async (isbn13) => {
   return await axios({
     method: "get",
-    url: `https://www.nl.go.kr/NL/search/openApi/search.do?key=${process.env.REACT_APP_NLK_API_KEY}&apiType=json&kwd=${isbn13}&pageSize=1&category=도서`,
+    url: `${NLK_HOST_URL}/NL/search/openApi/search.do?key=${NLK_API_KEY}&pageSize=1&category=도서&apiType=json&kwd=${isbn13}`,
   })
-};  
+}; 
+
+// 카카오 API: 검색을 통해 좌표값 수정
+export const getLocationsOnSearch = async (searchWord) => {
+  return await axios({
+    method: "get",
+    url: `${KAKAO_HOST_URL}/v2/local/search/address?&analyze_type=similar&size=20&query=${searchWord}`,
+    headers: {
+      Authorization: `KakaoAK ${KAKAO_REST_API_KEY}`
+    }
+  });
+};
