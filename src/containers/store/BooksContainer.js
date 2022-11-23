96483ff997ac3cdda777923f4f7abb352c087ffb
jiheon788 <jiheon788@ajou.ac.kr>
2022-11-24 05:22:33 +0900

    fix: 필터링 함수 오류

--- src/containers/store/BooksContainer.js
+++ src/containers/store/BooksContainer.js
@@ -4,50 +4,30 @@ import { useNavigate } from 'react-router-dom';
 import { getBooks } from "../../lib/api/book";
 
 // const filteredBooks
-const BooksContainer = () => {
+const BooksContainer = ({
+  filteredBooks
+}) => {
   const navigate = useNavigate()
-  const [totalBooks, setTotalBooks] = useState([]);
-  const [searchWord, setSearchWord] = useState("");
-
-  useEffect(() => {
-    getBooks().then(response => {
-      setTotalBooks(response.data.data);
-    }).catch(error => {
-      console.log(error)
-    })
-  }, [])
+  
 
   return (
     <>
-      <SearchBarContainer 
-        searchWord = {searchWord}
-        setSearchWord = {setSearchWord}
-      />
+      
+
       <div className="content-section">
         <div className='container grid-store'>
           {
-            Array.isArray(totalBooks) ?(
-              <>
-              {
-                totalBooks.filter((book) => {
-                  return book.title
-                    .replace(" ","")
-                    .toLocaleLowerCase()
-                    .includes(searchWord.toLocaleLowerCase().replace(" ",""))
-                }).map((book, index) => (
-                  <img 
-                    key={index} 
-                    src={book.thumbnail} 
-                    className="book-static book-sd btn-shadow" 
-                    alt="img"
-                    onClick={()=>{
-                      navigate(`/${book.isbn}/detail`)
-                    }} 
-                  />
-                ))
-              }
-              </>
-            ) : (<></>)
+            filteredBooks.map((book, index) => (
+              <img 
+                key={index} 
+                src={book.thumbnail} 
+                className="book-static book-sd btn-shadow" 
+                alt="img"
+                onClick={()=>{
+                  navigate(`/${book.isbn}/detail`)
+                }} 
+              />
+            ))
           }
         </div>
       </div>
--- src/pages/GenrePage.js
+++ src/pages/GenrePage.js
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import logo from '../assets/images/logo_row_white.png'
 import $ from 'jquery'
 import useScrollTo from "../lib/hooks/useScrollTo";
 import BooksContainer from "../containers/store/BooksContainer";
+import SearchBarContainer from "../containers/store/SeachBarContainer";
+import { useEffect, useState } from "react";
+import { getBooks } from "../lib/api/book";
 
 const GenrePage = () => {
   const {state} = useLocation();
@@ -15,6 +17,24 @@ const GenrePage = () => {
     setTimeout(() => {
       setIsScrollToStore(true);
     }, 1100)
+  }, [])
+
+  const [totalBooks, setTotalBooks] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
+
+  const filteredBooks = totalBooks.filter((book) => {
+    return book.title
+      .replace(" ","")
+      .toLocaleLowerCase()
+      .includes(searchWord.toLocaleLowerCase().replace(" ",""))
+  })
+  
+  useEffect(() => {
+    getBooks().then(response => {
+      setTotalBooks(response.data.data);
+    }).catch(error => {
+      console.log(error)
+    })
   }, [])
 
   
@@ -32,7 +52,11 @@ const GenrePage = () => {
 
     <div className="white-cement-bg" ref={storeRef}>
       <div className="gallery-area">
-        <BooksContainer />
+        <SearchBarContainer 
+          searchWord = {searchWord}
+          setSearchWord = {setSearchWord}
+        />
+        <BooksContainer filteredBooks={filteredBooks} />
       </div>
     </div>
     </>
