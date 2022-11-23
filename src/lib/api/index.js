bcf07a1274f7523e84841b74b4443d67e82c95cf
jiheon788 <jiheon788@ajou.ac.kr>
2022-11-23 16:15:54 +0900

    chore: 디버깅 코드

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const host = window.location.hostname === "localhost" ? 
-  process.env.REACT_APP_SERVER_URL : "api";
-
+const host = window.location.hostname === "localhost" ? process.env.REACT_APP_SERVER_URL : "api";
+console.log("host", host)
 export const apiClient = axios.create({
   baseURL: host,
 });
