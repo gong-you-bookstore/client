43f4836b5642964245bcf759c7558f53462e5950
jiheon788 <jiheon788@ajou.ac.kr>
2022-11-25 04:57:25 +0900

    style: 메인

--- src/components/home/GenreInfo.js
+++ src/components/home/GenreInfo.js
@@ -1,27 +1,51 @@
 import { CATEGORIES } from '../../lib/statics';
 import { useNavigate } from 'react-router-dom';
 import React from 'react';
+import { useCookies } from "react-cookie";
+import { toastMaker } from '../../lib/utils';
 
 const GenreInfo = () => {
+  const navigate = useNavigate();
+  const [cookies, setCookie, removeCookie] = useCookies(["userData"]);
+
   return (
     <div className="content-section">
         <div className="container">
           <div className='home-info-grid'>
           {
             CATEGORIES.map((category, index) => (
-              <React.Fragment key={index}>
-                <div 
-                className={`card${category.code} info-card`} />
-                <div className='info-form'>
-                  <p className='info-title'>
-                    {category.eng}
-                  </p>
-                  <p 
-                    className={`info-content info${category.code}`}
-                  >
-                  </p>
-                </div>
-              </React.Fragment>
+              <div
+                key={index}
+                className={`card${category.code} card `}
+                onClick={() => {
+                  if (!cookies.userData) {
+                    toastMaker.error("로그인이 필요합니다.")
+                    navigate('/login');
+                    return;
+                  }
+                  navigate(
+                    `/genre`, 
+                    { state: category }
+                  )
+                }}
+              >
+                <p className='fc-white '>
+                  {category.eng}
+                </p>
+              </div>
+              // <React.Fragment key={index}>
+              //   <div 
+              //   className={`card${category.code} info-card`} />
+              //   <div className='info-form'>
+              //     <p className='info-title'>
+              //       {category.eng}
+              //     </p>
+              //     <p 
+              //       className={`info-content info${category.code}`}
+              //     >
+              //     </p>
+              //   </div>
+              // </React.Fragment>
             ))
           }
           </div>
